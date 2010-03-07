8e9bf9cd4568e7c3ba8f497ef8c94d1c1be0b630
Tony Hillerson <tony.hillerson@effectiveui.com>
2010-03-07 16:07:30 -0700

    removing unused files til later

--- oauth_flow/gen/com/oreilly/android/otweet/R.java
+++ oauth_flow/gen/com/oreilly/android/otweet/R.java
@@ -10,26 +10,14 @@ package com.oreilly.android.otweet;
 public final class R {
     public static final class attr {
     }
-    public static final class color {
-        public static final int primary_black=0x7f050000;
-        public static final int window_title_background=0x7f050001;
-    }
     public static final class drawable {
-        public static final int btn_default=0x7f020000;
-        public static final int btn_default_normal=0x7f020001;
-        public static final int btn_default_normal_disable=0x7f020002;
-        public static final int btn_default_normal_disable_focused=0x7f020003;
-        public static final int btn_default_pressed=0x7f020004;
-        public static final int btn_default_selected=0x7f020005;
-        public static final int icon=0x7f020006;
-        public static final int list_item_background=0x7f020007;
-        public static final int list_item_background_gradient=0x7f020008;
+        public static final int icon=0x7f020000;
     }
     public static final class id {
-        public static final int authorize_button=0x7f080002;
-        public static final int enter_pin_text=0x7f080001;
-        public static final int enter_pin_title=0x7f080000;
-        public static final int web_view=0x7f080003;
+        public static final int authorize_button=0x7f060002;
+        public static final int enter_pin_text=0x7f060001;
+        public static final int enter_pin_title=0x7f060000;
+        public static final int web_view=0x7f060003;
     }
     public static final class layout {
         public static final int authorization_view=0x7f030000;
@@ -39,21 +27,10 @@ public final class R {
         public static final int oauth=0x7f040000;
     }
     public static final class string {
-        public static final int app_name=0x7f060004;
-        public static final int authorization=0x7f060000;
-        public static final int authorize=0x7f060001;
-        public static final int enter_pin=0x7f060002;
-        public static final int no_tweets=0x7f060003;
-    }
-    public static final class style {
-        public static final int TextAppearance=0x7f070000;
-        public static final int TextAppearance_WindowTitle=0x7f070001;
-        public static final int Theme_TwitterFirehose=0x7f070008;
-        public static final int Widget_Button=0x7f070004;
-        public static final int WindowTitle=0x7f070003;
-        public static final int WindowTitleBackground=0x7f070002;
-        public static final int user_detail_label=0x7f070007;
-        public static final int user_login_label=0x7f070006;
-        public static final int user_name_title=0x7f070005;
+        public static final int app_name=0x7f050004;
+        public static final int authorization=0x7f050000;
+        public static final int authorize=0x7f050001;
+        public static final int enter_pin=0x7f050002;
+        public static final int no_tweets=0x7f050003;
     }
 }
