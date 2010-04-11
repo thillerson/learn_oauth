3582d894b7a4801eca9f7a3d7d31882229f35f33
Tony Hillerson <tony.hillerson@gmail.com>
2010-04-10 22:51:44 -0600

    Great success. Uploading photos from photo library to Twitpic and posting message

--- photos/src/com/oreilly/android/otweet/activities/PostActivity.java
+++ photos/src/com/oreilly/android/otweet/activities/PostActivity.java
@@ -2,9 +2,13 @@ package com.oreilly.android.otweet.activities;
 
 import twitter4j.Status;
 
+import com.harrison.lee.twitpic4j.TwitPic;
+import com.harrison.lee.twitpic4j.TwitPicResponse;
 import com.oreilly.android.otweet.OTweetApplication;
 import com.oreilly.android.otweet.R;
+import com.oreilly.android.otweet.tasks.PostPhotoAsyncTask;
 import com.oreilly.android.otweet.tasks.PostTweetAsyncTask;
+import com.oreilly.android.otweet.tasks.PostPhotoAsyncTask.PostPhotoResponder;
 import com.oreilly.android.otweet.tasks.PostTweetAsyncTask.PostTweetResponder;
 
 import android.app.Activity;
@@ -13,7 +17,11 @@ import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.DialogInterface.OnClickListener;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.Menu;
@@ -25,7 +33,9 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class PostActivity extends Activity implements PostTweetResponder {
+public class PostActivity extends Activity implements PostTweetResponder, PostPhotoResponder {
+
+  private static final int REQUEST_CHOOSE_PHOTO_FROM_LIBRARY = 0;
 
   private OTweetApplication app;
   private TextView counterText;
@@ -34,6 +44,8 @@ public class PostActivity extends Activity implements PostTweetResponder {
   private ProgressDialog progressDialog;
   private Button photoButton;
 
+  private Uri photoURI;
+  
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
@@ -42,6 +54,15 @@ public class PostActivity extends Activity implements PostTweetResponder {
     setUpViews();
   }
   
+  @Override
+  protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+    if (REQUEST_CHOOSE_PHOTO_FROM_LIBRARY == requestCode) {
+      photoToPostChosenFromLibrary(intent.getData());
+    } else {
+      super.onActivityResult(requestCode, resultCode, intent);
+    }
+  }
+
   @Override
   protected void onResume() {
     super.onResume();
@@ -60,18 +81,40 @@ public class PostActivity extends Activity implements PostTweetResponder {
     return MenuHelper.openActivityFromMenuItem(this, item);
   }
   
-  public void tweetPosted(Status tweet) {
-    progressDialog.dismiss();
-    Toast.makeText(this, R.string.tweet_posted, Toast.LENGTH_LONG).show();
-    finish();
+  protected void photoToPostChosenFromLibrary(Uri uri) {
+    photoURI = uri;
+    String id = photoURI.getLastPathSegment();
+    if (null != id) {
+      photoButton.setText(null);
+      Bitmap thumbBitmap = MediaStore.Images.Thumbnails.getThumbnail(
+          getContentResolver(),
+          Long.parseLong(id),
+          MediaStore.Images.Thumbnails.MICRO_KIND,
+          null);
+      photoButton.setBackgroundDrawable(new BitmapDrawable(thumbBitmap));
+    }
   }
-
+  
+  public void photoPosting() {
+    tweetPosting();
+  }
+  
   public void tweetPosting() {
     progressDialog = ProgressDialog.show(
         this,
         getResources().getString(R.string.posting_title),
         getResources().getString(R.string.posting_description)
       );
+  }
+
+  public void tweetPosted(Status tweet) {
+    progressDialog.dismiss();
+    Toast.makeText(this, R.string.tweet_posted, Toast.LENGTH_LONG).show();
+    finish();
+  }
+
+  public void photoPosted(TwitPicResponse result) {
+    tweetPosted(null);
   }
 
   private void postValidTweetOrWarn() {
@@ -98,7 +141,12 @@ public class PostActivity extends Activity implements PostTweetResponder {
       }).
       show();
     } else {
-      new PostTweetAsyncTask(this, app.getTwitter()).execute(postText);
+      if (null == photoURI) {
+        new PostTweetAsyncTask(this, app.getTwitter()).execute(postText);
+      } else {
+        TwitPic twitpic = new TwitPic(app.getTwitPicUsername(), app.getTwitPicPassword());
+        new PostPhotoAsyncTask(this, this, twitpic).execute(postText, photoURI);
+      }
     }
   }
 
@@ -107,10 +155,31 @@ public class PostActivity extends Activity implements PostTweetResponder {
     postValidTweetOrWarn();
   }
   
+  protected void openCamera() {
+    
+  }
+
+  protected void openPhotoLibrary() {
+    Intent intent = new Intent(Intent.ACTION_GET_CONTENT, null);
+    intent.setType("image/*");
+    intent.addCategory(Intent.CATEGORY_OPENABLE);
+    startActivityForResult(intent, REQUEST_CHOOSE_PHOTO_FROM_LIBRARY);
+  }
+
   // called when post button on view is clicked
   public void photoButtonClicked(View view) {
     if (app.hasTwitPicCredentials()) {
-      // camera choices
+      new AlertDialog.Builder(this).
+        setTitle("Attach Photo").
+        setMessage("Choose a Photo Source").
+        setPositiveButton("Camera", cameraButtonClickListener).
+        setNeutralButton("Photo Library", libraryButtonClickListener).
+        setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+          public void onClick(DialogInterface dialog, int which) {
+            dialog.dismiss();
+          }
+        }).
+        show();
     } else {
       Intent intent = new Intent(this, SettingsActivity.class);
       startActivity(intent);
@@ -118,7 +187,9 @@ public class PostActivity extends Activity implements PostTweetResponder {
   }
   
   private void configurePhotoButton() {
-    if (app.hasTwitPicCredentials()) {
+    if (null != photoURI) {
+      photoButton.setText(null);
+    } else if (app.hasTwitPicCredentials()) {
       photoButton.setText(R.string.attach_photo);
     } else {
       photoButton.setText(R.string.sign_in_to_twitpic);
@@ -140,5 +211,16 @@ public class PostActivity extends Activity implements PostTweetResponder {
       }
     });
   }
+  
+  private OnClickListener cameraButtonClickListener = new DialogInterface.OnClickListener() {
+    public void onClick(DialogInterface dialog, int which) {
+      openCamera();
+    }
+  };
+  private OnClickListener libraryButtonClickListener = new DialogInterface.OnClickListener() {
+    public void onClick(DialogInterface dialog, int which) {
+      openPhotoLibrary();
+    }
+  };
 
 }
